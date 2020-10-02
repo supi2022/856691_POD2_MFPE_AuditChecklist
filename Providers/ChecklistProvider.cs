57a13e54a38add156e4d8e2aee747cea24eb742e
supriya sinha <supriyasinha4in@gmail.com>
2020-10-02 18:50:23 +0530

    Controller modified

--- Providers/ChecklistProvider.cs
+++ Providers/ChecklistProvider.cs
@@ -16,16 +16,15 @@ namespace AuditChecklistModule.Providers
             checklistRepoObj = _checklistRepoObj;
             _log4net = log4net.LogManager.GetLogger(typeof(ChecklistProvider));
         }
+
         List<Questions> listOfQuestions = new List<Questions>();
 
         public List<Questions> QuestionsProvider(string type)
         {
-            /*if (!type.Contains("Internal") && !type.Contains("SOX"))
-                return null;*/
             
             try
             {
-                _log4net.Info(" Http GET request called" + nameof(ChecklistProvider));
+                _log4net.Info(" Http GET request called " + nameof(ChecklistProvider));
                 listOfQuestions = checklistRepoObj.GetQuestions(type);
                 return listOfQuestions;
             }
@@ -34,8 +33,7 @@ namespace AuditChecklistModule.Providers
                 _log4net.Error("Exception " + e.Message + nameof(ChecklistProvider));
                 return null;
             }
-            
-            
+          
         }
     }
 }
